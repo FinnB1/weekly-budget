d9ba95efc1f44ffeb2ed108344f2dfa467a16d94
Finn <finnbutler1@gmail.com>
2020-10-17 21:11:15 +0100

    Updates

---
+++ precache-manifest.0a374dc51a5c6d5df014dc6636b1eaf9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ba9c0cbed6fe4e7faed54e480204f49",
+    "url": "/weekly-budget/index.html"
+  },
+  {
+    "revision": "3e7d2d346c0e2897d273",
+    "url": "/weekly-budget/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "acf2a7adfa0c07ba356f",
+    "url": "/weekly-budget/static/css/main.4f2ad67c.chunk.css"
+  },
+  {
+    "revision": "3e7d2d346c0e2897d273",
+    "url": "/weekly-budget/static/js/2.33c64ed8.chunk.js"
+  },
+  {
+    "revision": "7cd2e048a3be58e6922d93c69f118710",
+    "url": "/weekly-budget/static/js/2.33c64ed8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acf2a7adfa0c07ba356f",
+    "url": "/weekly-budget/static/js/main.2520b91e.chunk.js"
+  },
+  {
+    "revision": "51a69cfbc48cc4b4b7f8",
+    "url": "/weekly-budget/static/js/runtime-main.a2b1f8da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weekly-budget/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weekly-budget/precache-manifest.abe95ccae386dd55c29e682c3d308f47.js"
+  "/weekly-budget/precache-manifest.0a374dc51a5c6d5df014dc6636b1eaf9.js"
 );
 
 self.addEventListener('message', (event) => {
